658770e71054d618f8f45745be9b33ba58a579bb
Leonardo Peixoto <leonardo.peixoto@dock.tech>
2023-04-04 14:25:02 -0300

    feat: add add mock utils

---
+++ pkg/utils/mock/mock.go
@@ -0,0 +1,15 @@
+package mock
+
+import (
+	"github.com/stretchr/testify/mock"
+)
+
+func ReturnNilOrError(args mock.Arguments, index int) error {
+	if len(args) >= index {
+		if err, ok := args.Get(index).(error); ok {
+			return err
+		}
+	}
+
+	return nil
+}
