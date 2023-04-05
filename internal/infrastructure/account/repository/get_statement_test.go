cc0f517e60ff0679f6232e054b70ef0bca093192
Leonardo Peixoto <leonardo.peixoto@dock.tech>
2023-04-05 15:32:59 -0300

    test: add unit test to account repository
    test to GetStatement method

--- internal/infrastructure/account/repository/get_statement_test.go
+++ internal/infrastructure/account/repository/get_statement_test.go
@@ -1,1 +1,26 @@
 package repository
+
+import (
+	"context"
+	"testing"
+
+	"github.com/peixoto-leonardo/accounts/internal/infrastructure/postgres"
+	"github.com/peixoto-leonardo/accounts/pkg/utils/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetStatement(t *testing.T) {
+	t.Run("should return transactions of an account", func(t *testing.T) {
+		sqlMock := &postgres.SQLMock{}
+		rowsMock := &postgres.RowsMock{}
+		sqlMock.On("QueryContext").Return(rowsMock, nil)
+		rowsMock.On("Next").Return(false)
+		repository := New(sqlMock)
+
+		_, err := repository.GetStatement(context.TODO(), uuid.New())
+
+		assert.Nil(t, err)
+		sqlMock.AssertExpectations(t)
+		rowsMock.AssertExpectations(t)
+	})
+}
